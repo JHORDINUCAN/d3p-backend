1ca5f271ea359d068ff81209b158b60b4e9fe048
jhordin_ucan <jhordinalexander2015@gmail.com>
2025-04-07 10:45:25 -0500

    Configurado del ip

--- src/database.ts
+++ src/database.ts
@@ -3,8 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log("🚀 DB_HOST:", process.env.DB_HOST);
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || '127.0.0.1',
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
