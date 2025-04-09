04458a37ee64f712047a579f015f9603cca8e93a
jhordin_ucan <jhordinalexander2015@gmail.com>
2025-04-09 01:59:42 -0500

    Listo para produccion

--- src/routes/stripe.routes.ts
+++ src/routes/stripe.routes.ts
@@ -1,7 +1,11 @@
 import { Router, Request, Response } from 'express';
 import Stripe from 'stripe';
 
-const stripe = new Stripe('sk_test_51RBnSm0778qJCLUsmmZWLippF4nmKRGJRY86L9mY3Hy1XmyzgMVpajiOQ4OpAwzPmU9GKcNe5yNPM5JH4aY3fGF100rY0Pwg7l', { apiVersion: '2025-03-31.basil' });
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("La clave secreta de Stripe no está configurada en las variables de entorno.");
+}
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2025-03-31.basil' });
 const router = Router();
 
 router.post('/crear-pago', async (req: Request, res: Response): Promise<void> => {
@@ -19,10 +23,10 @@ router.post('/crear-pago', async (req: Request, res: Response): Promise<void> =>
     const amountInCents = Math.round(amount * 100);
 
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: amountInCents, 
-      currency: currency, 
-      payment_method: paymentMethodId, 
-      confirm: true, 
+      amount: amountInCents,
+      currency: currency,
+      payment_method: paymentMethodId,
+      confirm: true,
       automatic_payment_methods: {
         enabled: true,
         allow_redirects: 'never',
